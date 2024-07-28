084ef1e273f9f19d36d4f8c431321d3396daabb3
Lulu Cheng <l1990790120@users.noreply.github.com>
2024-07-27 22:51:42 -0400

    [change] add keyper section (#12)

--- page/components/intro/Contact.vue
+++ page/components/intro/Contact.vue
@@ -2,35 +2,10 @@
   <v-container class="page fill-height page pt-extra">
     <v-row>
       <v-col cols="12">
-        <span class="text-h2">How to get started?</span>
+        <span class="text-h2">Contact Us</span>
       </v-col>
 
       <v-row class="pt-10 justify-center">
-        <v-col cols="12">
-          <v-col>
-            <v-card>
-              <v-card-title>Keyper by Jarrid</v-card-title>
-              <v-card-text>
-                Our first open source crypto key automation library in
-                Kotlin/Java. Free. That's right. We don't want to waste time to
-                figure out pricing so we can focus on building the actual
-                security features for you. It's a super early release and we
-                know there will be issues. Let us know how we can fix it for
-                you. We love feedback. Even if you just want to talk.
-              </v-card-text>
-
-              <v-row class="py-3 px-3">
-                <v-col class="d-md-flex justify-end text-right">
-                  <div v-for="button in three_buttons" :key="button.text" class="pl-5 pt-3">
-                    <v-btn :href="button.to.href" target="_blank" class="cta-button" size="large">
-                      {{ button.text }}
-                    </v-btn>
-                  </div>
-                </v-col>
-              </v-row>
-            </v-card>
-          </v-col>
-        </v-col>
         <v-col cols="12" md="6">
           <v-col>
             <ContactCard :contact="contacts[0]" :socials="socials" />
@@ -53,40 +28,27 @@ export default {
   name: "Contact",
   data() {
     return {
-      three_buttons: [
-        {
-          text: "Demo",
-          to: { href: "https://jarrid.xyz/keyper/#demo" }
-        },
-        {
-          text: "Github",
-          to: { href: "https://github.com/jarrid-xyz/keyper" }
-        },
-        {
-          text: "Questions? Talk to Us",
-          to: { href: "https://calendar.app.google/qAjVcpLwLesdtSPQ8" }
-        }
-      ],
+
       contacts: [
         {
           name: "monday",
           title: "Follow Us",
           description: "#MondayFeatureDrop Stay tuned!",
-          social: true
+          social: true,
         },
         {
           name: "subscribe",
           title: "Subscribe",
           description: "Get weekly updates. Promised.",
-          subscribe: true
+          subscribe: true,
         },
         {
           name: "chat",
           title: "Chat with Us",
           description:
             "Not sure how to implement your security feature? Wanna tell us how our library doesn't work for you? We are always up for a chat.",
-          letstalk: true
-        }
+          letstalk: true,
+        },
       ],
       socials: [
         {
@@ -94,32 +56,32 @@ export default {
           title: "Stuff we've built. 100% free.",
           link: "https://github.com/jarrid-xyz",
           handle: "jarrid-xyz",
-          icon: "fab fa-github"
+          icon: "fab fa-github",
         },
         {
           name: "instagram",
           title: "Ask us anything. Weekly updates. Promised.",
           link: "https://www.instagram.com/jarrid.xyz",
           handle: "@jarrid.xyz",
-          icon: "fab fa-instagram"
+          icon: "fab fa-instagram",
         },
         {
           name: "linkedin",
           title: "Industry leading analysis.",
           link: "https://www.linkedin.com/company/apiobuild/",
           handle: "apiobuild",
-          icon: "fab fa-linkedin"
+          icon: "fab fa-linkedin",
         },
         {
           name: "email",
           title: "I will personally reply to all emails. Promised.",
           handle: "luluc@jarrid.xyz",
           link: "mailto:luluc@jarrid.xyz",
-          icon: "fas fa-envelope"
-        }
-      ]
+          icon: "fas fa-envelope",
+        },
+      ],
     };
-  }
+  },
 };
 </script>
 
